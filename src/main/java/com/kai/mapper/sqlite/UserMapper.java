352ec940c38701c8caf0d762daffb08757846d39
wangkai <285256241@qq.com>
2021-12-19 21:08:40 +0800

    annotation
    (cherry picked from commit 68f587f1655a13e0c5f7047e4f330a3dafa5e9ad)

--- src/main/java/com/kai/mapper/sqlite/UserMapper.java
+++ src/main/java/com/kai/mapper/sqlite/UserMapper.java
@@ -8,20 +8,41 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 /**
- * @author Wangkai
- * @description
- * @date 2021/12/18
- */
+ * @Author Wangkai
+ * @Description UserMapper
+ * @Date 2021/12/19 18:27
+ **/
 public interface UserMapper {
 
+    /**
+     * @Author Wangkai
+     * @Description getUser
+     * @Date 2021/12/19 18:27
+     * @Param [userId]
+     * @return com.kai.entity.User
+     **/
     @Select("select * from user where user_id = #{userId}")
     @ResultType(User.class)
     public User getUser(String userId);
 
+    /**
+     * @Author Wangkai
+     * @Description listUser
+     * @Date 2021/12/19 18:27
+     * @Param []
+     * @return java.util.List<com.kai.entity.User>
+     **/
     @Select("select * from user")
     @ResultType(User.class)
     public List<User> listUser();
 
+    /**
+     * @Author Wangkai
+     * @Description addUser
+     * @Date 2021/12/19 18:27
+     * @Param [user]
+     * @return int
+     **/
     @Insert({"insert into user values (#{userId}, #{userName}, #{gender}, #{age})"})
     public int addUser(User user);
 }
